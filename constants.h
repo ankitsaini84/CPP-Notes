95a26ad34de4b33d7f50c8abcffa50196c651cb5
Ankit Saini <ankit_saini@hotmail.com>
2020-12-27 00:17:30 +0530

    constants - namespace example

--- constants.h
+++ constants.h
@@ -2,7 +2,7 @@
 #define CONSTANTS_H
 
 namespace constants {   // User-defined namespace
-    constexpr double pi { 3.14 };
-    constexpr double gravity { 9.8 };
+    constexpr double pi { 3.14 };               // C++11/14
+    inline constexpr double gravity { 9.8 };    // Possible in C++17 onwards
 }
 #endif
