6cf3d3b082a8327c2573ba33cf5e15b0152446cc
Ankit Saini <ankit_saini@hotmail.com>
2020-12-27 20:24:51 +0530

    added comments

--- auto.cpp
+++ auto.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 
-auto add(auto a, auto b) {
+/*
+auto add(auto a, auto b) {  // TIP: Do NOT return 'auto' type. Also, C++20 interoduces taking function arguments as 'auto'.
+    return a + b;
+}*/
+
+int sum(const int a, const int b) {
     return a + b;
 }
 
+// TIP: Ideal use for 'auto' is when defining variables. e.g.
+auto g_sum { sum(2, 3) };
+
 int main() {
-    std::cout << add (4.4, 5.6);
+    std::cout << g_sum;
     return 0;
 }
