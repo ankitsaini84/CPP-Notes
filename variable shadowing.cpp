4193d52d96277b9c019b8aa070e18fc807176454
Ankit Saini <ankit_saini@hotmail.com>
2020-12-27 12:50:54 +0530

    global variable shadowing

--- variable shadowing.cpp
+++ variable shadowing.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 
+int x { 84 };                               // 'x' in GLOBAL SCOPE
+
 int main() {
-    int x { 3 };                            // Outer block 'x'
-    {                                       // Nested block
-        std::cout << "X : " << x << '\n';   // Prints value of outer block scope
+    // NOTE: Local variables with the same name as a global variable will shadow the global variable wherever the local variable is in scope.
+    int x { 30 };                           // MAIN block 'x' - It shadows 'x' in GLOBAL scope
+    {                                       // NESTED block
+        std::cout << "X : " << x << '\n';   // Prints value of MAIN block scope
 
-        // IMP: 'x' now refers to the nested block 'x' & outer block 'x' is temporarily HIDDEN.
-        int x {};                           // Defines 'x' inside nested block
-        std::cout << "X : " << x << '\n';   // Prints value of nested block scope
+        // IMP: 'x' now refers to the NESTED block 'x' & MAIN block 'x' is temporarily HIDDEN.
+        int x {};                           // NESTED block 'x' - It shadows 'x' in MAIN scope
+        std::cout << "X : " << x << '\n';   // Prints value of NESTED block scope
 
-        x = 5;                              // Assigns '3' to nested block 'x'
+        x = 5;                              // Assigns '3' to NESTED block 'x'
         std::cout << "X : " << x << '\n';
-    }                                       // Nested block destroyed
+    }                                       // NESTED block destroyed
 
-    std::cout << "X : " << x << '\n';       // Prints value of outer block scope
+    std::cout << "X : " << x << '\n';       // Prints value of 'x' in MAIN block scope
+    std::cout << "X : " << ::x << '\n';     // Prints value of 'x' in GLOBAL scope
 
     return 0;
 }
