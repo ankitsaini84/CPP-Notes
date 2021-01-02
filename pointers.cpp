67fc67e0987e5c89794f1b3d799fafbf2a985c88
Ankit Saini <ankit_saini@hotmail.com>
2021-01-02 17:21:24 +0530

    pointers & pointer arithmetic

---
+++ pointers.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <typeinfo>     // typeid()
+#include <cstddef>      // NULL // TIP: Avoid Using NULL
+
+// TIP: When declaring a function, put the asterisk of a pointer return 
+// value next to the type.
+int* foo1(int arr[]) {
+}   // IMP: Compiler implicitly converts (decays) arr[] to int*
+
+int foo2(int *arr) {    // TIP: Better Practice than previous notation.
+    *arr = 1;
+}   // IMP: Compiler implicitly converts (decays) *arr to int*
+
+int main() {
+    int x { 5 };
+    std::cout << "x : "     << x       << '\n';
+    std::cout << "&x : "    << &x      << '\n'; // Address-of operator [&] 
+    std::cout << "*(&x) : " << *(&x)   << '\n'; // Indirection/defrencing [*]
+
+    int *iPtr { &x };
+    // TIP: When declaring a pointer variable, put the asterisk next to 
+    // the variable name.
+    // Indirection through pointers
+    // NOTE: Indirection through a pointer evaluates to the contents of the 
+    // address it is pointing to. This is why pointers should have type.
+    // IMP: Without type, compiler wouldn't know how to interpret the 
+    // contents it is pointing to.
+    std::cout << "iPtr : "  << iPtr    << '\n';
+    std::cout << "*iPtr : " << *iPtr   << '\n';
+
+    // Invalid Assginments
+    // double *dPtr { iPtr };   // ERROR: int* != double*
+    // int *iPtr2 { x };        // ERROR: int* != int
+    
+    // IMP: Address-of operator returns a pointer.
+    std::cout << "Address-of operator type : " << typeid(&x).name() << '\n';
+
+    
+
+    // IMP: Size of the pointers.
+    // Size of the pointers is always the same, regardless what they are pointing to.
+    // On 32-bit machines - size of the pointer is 32 bits - 4 bytes.
+    // On 64-bit machines - size of the pointer is 64 bits - 8 bytes.
+    std::cout << "sizeof(iPtr) : " << sizeof(iPtr) << " bytes\n";
+    double *dPtr { nullptr };       // TIP: Best practice
+    std::cout << "sizeof(dPtr) : " << sizeof(dPtr) << " bytes\n";
+    int *iPtr3 { NULL };            // Another practice - Best use nullptr
+    int *iPtr4 { 0 };               // TIP: ALWAYS AVOID!
+
+
+
+    // ARRAY DECAY
+    constexpr int MAX { 5 };
+    int array[MAX] { 9, 8, 7, 6, 5 };
+    std::cout << "&array[0] : " << &array[0] << '\n';
+    std::cout << "array : "     << array << '\n';
+
+    std::cout << "*array : "    << *array << '\n';
+    std::cout << "array[0] : "  << array[0] << '\n';
+    iPtr = array;
+    std::cout << "*iPtr : "     << *iPtr << '\n';
+    foo2(array);    // IMP: Call by Address - 'array' will decay to 'int*'
+    std::cout << "array[0] : "  << array[0] << '\n';
+    /**
+     * IMP: It's a common fallacy to belive that an array & a pointer are 
+     * identical. They are not!
+     * Above, array is of type 'int[5]', & its value is the array element 
+     * themselves.
+     * A pointer to the array would be of type 'int*', & its value would be
+     * the address of the first element of the array.
+     * This works, because the array decays into a pointer of type int*, & 
+     * our pointer also has the same type (i.e. int*).
+     */
+    // To prove these 2 are different.. print their respective sizes.
+    std::cout << "sizeof(array) : " << sizeof(array) << '\n';
+    // Statement above, prints the size of the array as 20.
+    std::cout << "sizeof(iPtr) : "  << sizeof(iPtr) << '\n';
+    // Statement above, prints the size of the pointer as 8.
+
+
+
+    // POINTER ARITHMETIC
+    // NOTE: pointer + 1 returns the memory address of the next object of 
+    // the type that pointer points to.
+    // Example : 
+    const int length = sizeof(array) / sizeof(array[0]);
+    for (int i = 0; i < length; ++i) {
+        std::cout << "iPtr + " << i << " : " << iPtr + i
+        << " : " << *(iPtr + i) << '\n';
+    }
+    // Above, each iteration prints the address of subsequent elements in 
+    // the array as the iPtr points to int (4 bytes) and +1 makes it point 
+    // to the next element in the array.
+
+    return 0;
+}
